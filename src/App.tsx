a1a8c6c8f305804f6f9b21c2b8d091b19a90801a
Diego Padilla <diegopadilla672@gmail.com>
2026-07-18 16:52:16 +0200

    feat(router): add React Router structure to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Login from './login'
+import AppProviders from './context/AppProviders'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Test from './Test'
+import Test2 from './Test2'
 
 function App() {
 
   return (
-    <>
-      <Login />
-    </>
+    <AppProviders>
+      <HashRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Test />} />
+          </Route>
+          <Route>
+            <Route index path="/task" element={<Test2 />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </AppProviders>
   )
 }
 
