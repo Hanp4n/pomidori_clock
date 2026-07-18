cc2c937fe4680bf25a77929f04251599d66c7cd0
Diego Padilla <diegopadilla672@gmail.com>
2026-07-18 19:11:01 +0200

    feat(db): add types to local and remote databases schemas

---
+++ src/db/schema.postgres.ts
@@ -0,0 +1,123 @@
+export type RemoteUser = {
+  id: string;
+  username: string;
+  email: string;
+  created_at: string;
+};
+export type NewRemoteUser = {
+  id?: string;
+  username: string;
+  email: string;
+  created_at: string;
+};
+export type RemotePomodoroConfig = {
+  id: string;
+  user_id: string;
+  focus_time: number;
+  short_break_time: number;
+  long_break_time: number;
+  created_at: string;
+  updated_at: string;
+};
+export type NewRemotePomodoroConfig = {
+  id?: string;
+  user_id?: string | null;
+  focus_time: number;
+  short_break_time: number;
+  long_break_time: number;
+  created_at: string;
+  updated_at: string;
+};
+export type RemoteTask = {
+  id: string;
+  user_id: string;
+  title: string;
+  description: string | null;
+  n_pomodoros: number;
+  completed_pomodoros: number;
+  is_completed: boolean;
+  updated_at: string;
+  created_at: string;
+};
+export type NewRemoteTask = {
+  id?: string;
+  user_id: string;
+  title: string;
+  description?: string | null;
+  n_pomodoros?: number;
+  completed_pomodoros?: number;
+  is_completed?: boolean;
+  updated_at: string;
+  created_at: string;
+};
+export type RemotePomodoroSession = {
+  id: string;
+  user_id: string;
+  task_id: string | null;
+  type: 'focus' | 'short_break' | 'long_break';
+  duration: number;
+  updated_at: string;
+  started_at: string;
+  ended_at: string | null;
+};
+export type NewRemotePomodoroSession = {
+  id?: string;
+  user_id: string;
+  task_id?: string | null;
+  type: 'focus' | 'short_break' | 'long_break';
+  duration?: number;
+  updated_at: string;
+  started_at: string;
+  ended_at?: string | null;
+};
+export type RemoteCategory = {
+  id: string;
+  user_id: string;
+  name: string;
+  color: string;
+  updated_at: string;
+  created_at: string;
+};
+export type NewRemoteCategory = {
+  id?: string;
+  user_id: string;
+  name: string;
+  color?: string;
+  updated_at: string;
+  created_at: string;
+};
+export type RemoteTaskCategory = {
+  id: string;
+  task_id: string;
+  category_id: string;
+  created_at: string;
+};
+export type NewRemoteTaskCategory = {
+  id?: string;
+  task_id: string;
+  category_id: string;
+  created_at: string;
+};
+export type RemoteRequest = {
+  id: string;
+  sender_id: string;
+  receiver_id: string;
+  type: 'friend' | 'co_study';
+  created_at: string;
+};
+export type NewRemoteRequest = {
+  id?: string;
+  sender_id: string;
+  receiver_id: string;
+  type: 'friend' | 'co_study';
+  created_at: string;
+};
+export type RemoteFriend = {
+  user_id: string;
+  friend_id: string;
+};
+export type NewRemoteFriend = {
+  user_id: string;
+  friend_id: string;
+};
+
---
+++ src/db/schema.sqlite.ts
@@ -0,0 +1,140 @@
+export type LocalUser = {
+  id: string;
+  username: string | null;
+  email: string | null;
+  is_guest: 1 | 0;
+  access_token: string | null;
+  refresh_token: string | null;
+  created_at: string;
+};
+
+export type NewLocalUser = {
+  id: string;
+  username?: string | null;
+  email?: string | null;
+  access_token?: string | null;
+  refresh_token?: string | null;
+  is_guest?: 1 | 0;
+  created_at?: string;
+};
+
+export type LocalPomodoroConfig = {
+  id: string;
+  user_id: string;
+  focus_time: number;
+  short_break_time: number;
+  long_break_time: number;
+  created_at: string;
+  updated_at: string;
+  deleted_at?: string | null;
+  is_synced: 1 | 0;
+};
+
+export type NewLocalPomodoroConfig = {
+  id: string;
+  user_id: string;
+  focus_time?: number;
+  short_break_time?: number;
+  long_break_time?: number;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string | null;
+  is_synced?: 1 | 0;
+};
+
+export type LocalTask = {
+  id: string;
+  user_id: string;
+  title: string;
+  description: string | null;
+  n_pomodoros: number;
+  completed_pomodoros: number;
+  is_completed: 1 | 0;
+  updated_at: string;
+  created_at: string;
+  deleted_at: string | null;
+  is_synced: 1 | 0;
+};
+
+export type NewLocalTask = {
+  id: string;
+  user_id: string;
+  title: string;
+  description: string | null;
+  n_pomodoros?: number;
+  completed_pomodoros?: number;
+  is_completed?: 1 | 0;
+  updated_at?: string;
+  created_at?: string;
+  deleted_at?: string | null;
+  is_synced?: 1 | 0;
+};
+
+export type LocalPomodoroSession = {
+  id: string;
+  user_id: string;
+  task_id: string | null;
+  type: 'focus' | 'short_break' | 'long_break';
+  duration: number;
+  started_at: string;
+  ended_at: string | null;
+  updated_at: string;
+  is_synced: 1 | 0;
+};
+
+export type NewLocalPomodoroSession = {
+  id: string;
+  user_id: string;
+  type: 'focus' | 'short_break' | 'long_break';
+  started_at: string;
+  task_id?: string | null;
+  duration?: number;
+  ended_at?: string | null;
+  updated_at?: string;
+  is_synced?: 1 | 0;
+};
+
+export type LocalCategory = {
+  id: string;
+  user_id: string;
+  name: string;
+  color: string;
+  updated_at: string;
+  created_at: string;
+  deleted_at: string | null;
+  is_synced: 1 | 0;
+};
+
+export type NewLocalCategory = {
+  id: string;
+  user_id: string;
+  name: string;
+  color?: string;
+  updated_at?: string;
+  created_at?: string;
+  deleted_at?: string | null;
+  is_synced?: 1 | 0;
+};
+
+export type LocalTaskCategory = {
+  id: string;
+  task_id: string;
+  category_id: string;
+  created_at: string;
+  deleted_at: string | null;
+  is_synced: 1 | 0;
+};
+
+export type NewLocalTaskCategory = {
+  id: string;
+  task_id: string;
+  category_id: string;
+  created_at?: string;
+  deleted_at?: string | null;
+  is_synced?: 1 | 0;
+};
+
+export type LocalAppState = {
+  id: number;
+  active_user_id: string | null;
+};
