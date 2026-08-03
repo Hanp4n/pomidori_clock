43e35efd426163ea6f2580bed2203f69a96ab31e
Diego Padilla <diegopadilla672@gmail.com>
2026-08-03 16:28:47 +0200

    chore(router): remove auth testing environment from router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import AppProviders from './context/AppProviders'
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Test from './Test'
 import Test2 from './Test2'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
+import OfflineLogin from './pages/auth/OfflineLogin'
 
 function App() {
 
@@ -14,8 +14,8 @@ function App() {
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/offline" element={<OfflineLogin />} />
           <Route path="/task" element={<Test2 />} />
-          <Route path="/test" element={<Test />} />
         </Routes>
       </HashRouter>
     </AppProviders>
