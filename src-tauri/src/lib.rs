98fc5561f3820ebf6c28d4bb50812eeba9b6cc83
Diego Padilla <diegopadilla672@gmail.com>
2026-08-24 11:36:59 +0100

    feat(tauri): add Shell and Notifications plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -40,8 +40,10 @@ pub fn run() {
                 .add_migrations("sqlite:pomidori_clock_local.db", migrations)
                 .build(),
         )
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_keyring::init())
         .plugin(tauri_plugin_connectivity::init())
+        .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
